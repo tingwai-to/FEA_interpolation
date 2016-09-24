343c471a8d98c77efe82c7d782d64dc7905297bb
tingwai-to <tingwai.to@gmail.com>
2016-09-24 00:07:06 -0500

    Interpolation functions return None if outside element. Put test points in separate file

--- interpolate.py
+++ interpolate.py
@@ -1,9 +1,8 @@
 import numpy as np
 import numpy.linalg as npla
-# import matplotlib.pyplot as plt
-# import sys
 
-def interpolate3D(p1, p2, p3, p4, p5, v1, v2, v3, v4):
+
+def interpolate3d(p1, p2, p3, p4, point, v1, v2, v3, v4):
     J = np.array([[p2[0]-p1[0], p3[0]-p1[0], p4[0]-p1[0]],
                   [p2[1]-p1[1], p3[1]-p1[1], p4[1]-p1[1]],
                   [p2[2]-p1[2], p3[2]-p1[2], p4[2]-p1[2]]])
@@ -13,21 +12,28 @@ def interpolate3D(p1, p2, p3, p4, p5, v1, v2, v3, v4):
     # ref2 = J.dot(np.array([ p2[0]-p1[0], p2[1]-p1[1], p2[2]-p1[2] ]))
     # ref3 = J.dot(np.array([ p3[0]-p1[0], p3[1]-p1[1], p3[2]-p1[2] ]))
     # ref4 = J.dot(np.array([ p4[0]-p1[0], p4[1]-p1[1], p4[2]-p1[2] ]))
-    ref5 = J.dot(np.array([ p5[0]-p1[0], p5[1]-p1[1], p5[2]-p1[2] ]))
+    ref_point = J.dot(np.array([ point[0]-p1[0], point[1]-p1[1], point[2]-p1[2] ]))
 
-    tot_vol = 1./6  # volume of trirectangular tetrahedron
+    tot_vol = 1./6  # Volume of trirectangular tetrahedron
     # Volume of tetrahedron = 1/3 * base_area * height
-    vol2 = (1./3)*(1./2)*ref5[0]
-    vol3 = (1./3)*(1./2)*ref5[1]
-    vol4 = (1./3)*(1./2)*ref5[2]
-    vol1 = tot_vol - vol2 -vol3 - vol4
+    vol2 = (1./3)*(1./2)*ref_point[0]
+    vol3 = (1./3)*(1./2)*ref_point[1]
+    vol4 = (1./3)*(1./2)*ref_point[2]
+    vol1 = tot_vol - vol2 - vol3 - vol4
 
-    v5 = v1*(vol1/tot_vol) + v2*(vol2/tot_vol) + v3*(vol3/tot_vol) + v4*(vol4/tot_vol)
+    lam1 = vol1/tot_vol
+    lam2 = vol2/tot_vol
+    lam3 = vol3/tot_vol
+    lam4 = vol4/tot_vol
 
-    print v5
-    return v5
+    if (0<lam1<1 and 0<lam2<1 and 0<lam3<1 and 0<lam4<1):
+        v_point = v1*(vol1/tot_vol) + v2*(vol2/tot_vol) + v3*(vol3/tot_vol) + v4*(vol4/tot_vol)
+        return v_point
+    else:
+        return None
 
-def interpolate2D(p1, p2, p3, p4, v1, v2, v3):
+
+def interpolate2d(p1, p2, p3, point, v1, v2, v3):
     trans = np.array([[ p2[0]-p1[0], p3[0]-p1[0] ],
                       [ p2[1]-p1[1], p3[1]-p1[1] ]])
     trans = npla.inv(trans)
@@ -35,64 +41,19 @@ def interpolate2D(p1, p2, p3, p4, v1, v2, v3):
     # ref1 = trans.dot(np.array([ p1[0]-p1[0], p1[1]-p1[1] ]))
     # ref2 = trans.dot(np.array([ p2[0]-p1[0], p2[1]-p1[1] ]))
     # ref3 = trans.dot(np.array([ p3[0]-p1[0], p3[1]-p1[1] ]))
-    ref4 = trans.dot(np.array([ p4[0]-p1[0], p4[1]-p1[1] ]))
+    ref_point = trans.dot(np.array([ point[0]-p1[0], point[1]-p1[1] ]))
 
-    tot_area = 0.5  # area of right unit triangle
-    area2 = 0.5*1*ref4[0]
-    area3 = 0.5*1*ref4[1]
+    tot_area = 0.5  # Area of 45-45-90 triangle, side length=1
+    area2 = 0.5*1*ref_point[0]
+    area3 = 0.5*1*ref_point[1]
     area1 = tot_area - area2 - area3
 
-    v4 = v1*(area1/tot_area) + v2*(area2/tot_area) + v3*(area3/tot_area)
-    # Anywhere outside of reference triangle, set to zero
+    lam1 = area1/tot_area
+    lam2 = area2/tot_area
+    lam3 = area3/tot_area
 
-    print v4
-    return v4
-
-
-# 2D sample points
-p1 = np.array([2,2])
-p2 = np.array([4,3])
-p3 = np.array([1,4])
-p4 = np.array([2.5, 2.5])  # Value to be interpolated
-v1 = 1
-v2 = 2
-v3 = 12
-
-interpolate2D(p1, p2, p3, p4, v1, v2, v3)
-
-# 3D sample points
-p1 = np.array([2,2,1])
-p2 = np.array([4,3,5])
-p3 = np.array([1,4,3])
-p4 = np.array([6,5,3])
-p5 = np.array([3,3.5,3]) # Value to be interpolated
-v1 = 1
-v2 = 3
-v3 = 12
-v4 = 20
-
-interpolate3D(p1, p2, p3, p4, p5, v1, v2, v3, v4)
-
-# sys.exit()
-
-x_min = min(_[0] for _ in (p1, p2, p3))
-y_min = min(_[1] for _ in (p1, p2, p3))
-x_max = max(_[0] for _ in (p1, p2, p3))
-y_max = max(_[1] for _ in (p1, p2, p3))
-
-N = 128
-buff = np.zeros((N,N), dtype="f8")
-
-x, y = np.mgrid[x_min:x_max:N*1j, y_min:y_max:N*1j]
-p4 = np.array([x.ravel(), y.ravel()])
-buff = interpolate2D(p1, p2, p3, p4, v1, v2, v3).reshape((N, N)).T
-print buff
-
-import matplotlib
-matplotlib.use("agg")
-import matplotlib.pyplot as plt
-
-plt.imshow(buff, extent=[x_min, x_max, y_min, y_max], origin='lower')
-plt.plot([p1[0], p2[0], p3[0], p1[0]], [p1[1], p2[1], p3[1], p1[1]], '-k')
-plt.colorbar()
-plt.savefig("output.png")
+    if (0<lam1<1 and 0<lam2<1 and 0<lam3<1):
+        v_point = v1*lam1 + v2*lam2 + v3*lam3
+        return v_point
+    else:
+        return None
---
+++ sample_points.py
@@ -0,0 +1,47 @@
+import interpolate
+import numpy as np
+
+
+# 2D sample points
+p1 = np.array([2,2])
+p2 = np.array([4,3])
+p3 = np.array([1,4])
+p4 = np.array([2,2])  # Value to be interpolated
+v1 = 1
+v2 = 2
+v3 = 3
+
+
+# 3D sample points
+# p1 = np.array([2,2,1])
+# p2 = np.array([4,3,5])
+# p3 = np.array([1,4,3])
+# p4 = np.array([6,5,3])
+# p5 = np.array([3,3.5,3]) # Value to be interpolated
+# v1 = 1
+# v2 = 2
+# v3 = 3
+# v4 = 4
+
+
+x_min = min(_[0] for _ in (p1, p2, p3))
+y_min = min(_[1] for _ in (p1, p2, p3))
+x_max = max(_[0] for _ in (p1, p2, p3))
+y_max = max(_[1] for _ in (p1, p2, p3))
+
+
+N = 128
+buff = np.zeros((N, N), dtype="f8")
+for i, x in enumerate(np.linspace(x_min,x_max,N)):
+    for j, y in enumerate(np.linspace(y_min,y_max,N)):
+        buff[i][j] = interpolate.interpolate2D(p1,p2,p3,np.array([x,y]),v1,v2,v3)
+
+
+import matplotlib
+matplotlib.use("agg")
+import matplotlib.pyplot as plt
+
+plt.plot([p1[0], p2[0], p3[0], p1[0]], [p1[1], p2[1], p3[1], p1[1]], '-k')
+plt.imshow(buff.T, extent=[x_min, x_max, y_min, y_max], origin='lower')
+plt.colorbar()
+plt.savefig("output2D.png")
