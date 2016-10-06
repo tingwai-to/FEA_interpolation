603adec6c2630048a8df39e3cee49327514d3703
tingwai_to <tingwai.to@gmail.com>
2016-10-06 05:48:37 -0500

    Adding 3d jit for float64, unfinished

--- interpolate.py
+++ interpolate.py
@@ -4,16 +4,47 @@ import numpy.linalg as npla
 from numba import jit
 import numba as nb
 
-def interpolate3d(p1, p2, p3, p4, point, v1, v2, v3, v4):
+
+@jit(nb.float64[:](nb.float64[:], nb.float64[:], nb.float64[:], nb.float64[:], nb.float64[:,:],
+     nb.float64, nb.float64, nb.float64, nb.float64),
+     nopython=True)
+# UNFINISHED
+def interpolate3d_f64(p1, p2, p3, p4, point, v1, v2, v3, v4):
     J = np.array([[p2[0]-p1[0], p3[0]-p1[0], p4[0]-p1[0]],
                   [p2[1]-p1[1], p3[1]-p1[1], p4[1]-p1[1]],
                   [p2[2]-p1[2], p3[2]-p1[2], p4[2]-p1[2]]])
     J = npla.inv(J)
 
-    # ref1 = J.dot(np.array([ p1[0]-p1[0], p1[1]-p1[1], p1[2]-p1[2] ]))
-    # ref2 = J.dot(np.array([ p2[0]-p1[0], p2[1]-p1[1], p2[2]-p1[2] ]))
-    # ref3 = J.dot(np.array([ p3[0]-p1[0], p3[1]-p1[1], p3[2]-p1[2] ]))
-    # ref4 = J.dot(np.array([ p4[0]-p1[0], p4[1]-p1[1], p4[2]-p1[2] ]))
+    ref_point = np.empty_like(point)
+    for i in range(point.shape[0]):
+        for j in range(point.shape[1]):
+            ref_point[i,j] = J[i,0]*(point[0,j]-p1[i]) + \
+                             J[i,1]*(point[1,j]-p1[i]) + \
+                             J[i,2]*(point[2,j]-p1[i])
+
+    tot_vol = 1./6  # Volume of trirectangular tetrahedron
+    # Volume of tetrahedron = 1/3 * base_area * height
+    vol2 = (1./3)*(1./2)*ref_point[0]
+    vol3 = (1./3)*(1./2)*ref_point[1]
+    vol4 = (1./3)*(1./2)*ref_point[2]
+    vol1 = tot_vol - vol2 - vol3 - vol4
+
+    v_point = v1*(vol1/tot_vol) + v2*(vol2/tot_vol) + v3*(vol3/tot_vol) + v4*(vol4/tot_vol)
+
+    mask = np.ones_like(v_point, dtype="bool")
+    for v in [vol1, vol2, vol3, vol4]:
+        mask &= (v/tot_vol) > 0
+        mask &= (v/tot_vol) < 1
+
+    return v_point#, mask
+
+
+def interpolate3d_nojit64(p1, p2, p3, p4, point, v1, v2, v3, v4):
+    J = np.array([[p2[0]-p1[0], p3[0]-p1[0], p4[0]-p1[0]],
+                  [p2[1]-p1[1], p3[1]-p1[1], p4[1]-p1[1]],
+                  [p2[2]-p1[2], p3[2]-p1[2], p4[2]-p1[2]]])
+    J = npla.inv(J)
+
     ref_point = J.dot(np.array([ point[0]-p1[0], point[1]-p1[1], point[2]-p1[2] ]))
 
     tot_vol = 1./6  # Volume of trirectangular tetrahedron
@@ -32,9 +63,10 @@ def interpolate3d(p1, p2, p3, p4, point, v1, v2, v3, v4):
 
     return v_point, mask
 
+
 @jit(nb.float64[:](nb.float64[:], nb.float64[:], nb.float64[:], nb.float64[:,:],
-    nb.float64, nb.float64, nb.float64),
-    nopython=True)
+     nb.float64, nb.float64, nb.float64),
+     nopython=True)
 def interpolate2d_f64(p1, p2, p3, point, v1, v2, v3):
     # Transformation matrix to reference element
     trans = np.empty((2,2), dtype=nb.float64)
@@ -70,9 +102,10 @@ def interpolate2d_f64(p1, p2, p3, point, v1, v2, v3):
             mask[i] = 0
     return v_point#, mask
 
+
 @jit(nb.float32[:](nb.float32[:], nb.float32[:], nb.float32[:], nb.float32[:,:],
-    nb.float32, nb.float32, nb.float32),
-    nopython=True)
+     nb.float32, nb.float32, nb.float32),
+     nopython=True)
 def interpolate2d_f32(p1, p2, p3, point, v1, v2, v3):
     # Transformation matrix to reference element
     trans = np.empty((2,2), dtype=nb.float32)
@@ -98,6 +131,7 @@ def interpolate2d_f32(p1, p2, p3, point, v1, v2, v3):
 
     return v_point
 
+
 def interpolate2d_nojit64(p1, p2, p3, point, v1, v2, v3):
     trans = np.array([[ p2[0]-p1[0], p3[0]-p1[0] ],
                       [ p2[1]-p1[1], p3[1]-p1[1] ]])
@@ -118,6 +152,7 @@ def interpolate2d_nojit64(p1, p2, p3, point, v1, v2, v3):
         mask *= (a/tot_area) < 1
 
     return v_point#, mask
+
 
 def interpolate2d_nojit32(p1, p2, p3, point, v1, v2, v3):
     trans = np.array([[ p2[0]-p1[0], p3[0]-p1[0] ],
