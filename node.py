d9662016dbe0af58618427e2d96e1dfa62903480
tingwai-to <tingwai.to@gmail.com>
2016-10-15 09:06:29 -0500

    Added Node2D class

---
+++ node.py
@@ -0,0 +1,11 @@
+from __future__ import print_function
+
+class Node2D(object):
+    def __init__(self, coord, value):
+        self.coord = coord
+        self.value = value
+
+class Node3D(object):
+    def __init__(self, coord, value):
+        self.coord = coord
+        self.value = value
