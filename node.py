9a3fa092c545e5bf3deed13929625f68f96c638a
tingwai_to <tingwai.to@gmail.com>
2016-10-17 23:51:49 -0500

    Node2D, Node3D now subclass of Node

--- node.py
+++ node.py
@@ -1,11 +1,16 @@
 from __future__ import print_function
 
-class Node2D(object):
+class Node(object):
     def __init__(self, coord, value):
         self.coord = coord
         self.value = value
 
-class Node3D(object):
+
+class Node3D(Node):
     def __init__(self, coord, value):
-        self.coord = coord
-        self.value = value
+        super(Node3D, self).__init__(coord, value)
+
+
+class Node2D(Node):
+    def __init__(self, coord, value):
+        super(Node2D, self).__init__(coord, value)
