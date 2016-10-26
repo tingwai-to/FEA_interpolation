1d0598fdb631baa62f97d896a8e05e0d60a352a3
tingwai_to <tingwai.to@gmail.com>
2016-10-26 00:24:46 -0500

    remove idw.py, linear.py

--- idw.py
+++
@@ -1,5 +0,0 @@
-from __future__ import print_function
-import numpy as np
-from numpy import sqrt
-from numba import jit
-import numba as nb
--- linear.py
+++
@@ -1,5 +0,0 @@
-from __future__ import print_function
-import numpy as np
-import numpy.linalg as npla
-from numba import jit
-import numba as nb
