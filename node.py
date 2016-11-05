d469564b9c448ea3531dcc61545c7862a926c261
Ting-Wai To <tingwai.to@gmail.com>
2016-11-04 23:43:56 -0500

    Revert "N dim interpolation"

--- node.py
+++ node.py
@@ -2,7 +2,6 @@ from __future__ import print_function
 
 class Node(object):
     def __init__(self, coord, value):
-        self.dim = coord.size
         self.coord = coord
         self.value = value
 
