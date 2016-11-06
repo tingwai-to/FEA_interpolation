Re-committing broadcasting stuff to correct branch

element.py
@@ -14,10 +14,10 @@ class Element(object):
         self.dim = nodes[0].dim
 
         #: list of np.array: coordinates of nodes
-        self.coords = [thisnode.coord for thisnode in nodes]
+        self.coords = np.array([thisnode.coord for thisnode in nodes])
 
         #: list of float: values of nodes
-        self.values = [thisnode.value for thisnode in nodes]
+        self.values = np.array([thisnode.value for thisnode in nodes])
 
     def sample(self, method, point, *args, **kwargs):
         use_jit = kwargs.pop("jit", False)
@@ -33,15 +33,9 @@ class Element(object):
     def linear_nojit(self, point):
         """Non-JIT linear interpolation for nD element"""
         dtype = point.dtype
-
-        trans = np.empty((self.dim, self.dim), dtype=dtype)
-        for i in range(self.dim):
-            for j in range(self.dim):
-                trans[i,j] = self.coords[j+1][i] - self.coords[0][i]
+        trans = (self.coords[1:,:] - self.coords[0,:]).T
         trans = npla.inv(trans)
-        ref_point = trans.dot(np.array(
-            [point[j]-self.coords[0][j] for j in range(self.dim)],
-            dtype=dtype))
+        ref_point = trans.dot((point-self.coords[0,:][:,None]))
 
         tot_vol = np.array([1./factorial(self.dim)], dtype=dtype)
         vols = []
@@ -527,43 +521,6 @@ def idw_simple_2d(p1, p2, p3, point, v1, v2, v3, power):
                weight_2d(p3, point, power))
     return v_point
 
-"""
-# TODO: cuda functions are WIP
-@cuda.jit(nb.float64(nb.float64[:], nb.float64[:], nb.float64[:],
-                     nb.float64[:],
-                     nb.float64, nb.float64, nb.float64,
-                     nb.float64[:,:]),
-          device=True)
-def linear_2d_cuda(p1, p2, p3, point, v1, v2, v3, trans):
-    return point[0]     # check visualization
-    ref_point_x = trans[0,0]*(point[0]-p1[0]) + trans[0,1]*(point[1]-p1[0])
-    ref_point_y = trans[1,0]*(point[0]-p1[1]) + trans[1,1]*(point[1]-p1[1])
-
-    area2 = 0.5*ref_point_x
-    area3 = 0.5*ref_point_y
-    area1 = 0.5 - area2 - area3
-
-    if (area1/0.5) < 0 or \
-       (area1/0.5) > 1 or \
-       (area2/0.5) < 0 or \
-       (area2/0.5) > 1 or \
-       (area3/0.5) < 0 or \
-       (area3/0.5) > 1:
-        return -1
-    else:
-        return v1*(area1/0.5) + v2*(area2/0.5) + v3*(area3/0.5)
-
-
-@cuda.jit(nb.void(nb.float64[:,:],
-                  nb.float64[:], nb.float64[:], nb.float64[:],
-                  nb.float64[:,:,:],
-                  nb.float64, nb.float64, nb.float64,
-                  nb.float64[:,:]))
-def make_cuda(result, p1, p2, p3, point, v1, v2, v3, trans):
-    i = cuda.grid(1)
-    for j in range(result[1].size):
-        result[i, j] = linear_2d_cuda(p1, p2, p3, point[i, j], v1, v2, v3, trans)
-"""
 
 jit_functions = {}
 

sample_2d_points.py
@@ -43,42 +43,6 @@ points_f32 = np.array([_.ravel() for _ in (x, y)], dtype='f')
 power = 128 # to exaggerate visualization of IDW
 
 
-def test_cuda():
-    p1 = triangle.p1
-    p2 = triangle.p2
-    p3 = triangle.p3
-    v1 = triangle.v1
-    v2 = triangle.v2
-    v3 = triangle.v3
-    N = 1024
-    x, y = np.mgrid[x_min:x_max:1j*N,
-                    y_min:y_max:1j*N]
-
-    trans = np.empty((2,2), dtype='f8')
-    trans[0,0] = p2[0] - p1[0]
-    trans[0,1] = p3[0] - p1[0]
-    trans[1,0] = p2[1] - p1[1]
-    trans[1,1] = p3[1] - p1[1]
-    trans = np.linalg.inv(trans)
-
-    point = np.array([_.ravel() for _ in (x, y)], dtype='f8')
-    point = point.reshape((N,N,2))
-    result = np.empty((N,N), dtype='f8')
-
-    start = time()
-    element.make_cuda[1,N](result, p1, p2, p3, point, v1, v2, v3, trans)
-    end = time()
-    print(end-start)
-    print(result)
-
-    plt.figure()
-    plt.imshow(result.T, extent=[x_min, x_max, y_min, y_max], origin='lower',
-               interpolation='nearest')
-    plt.plot([p1[0], p2[0], p3[0], p1[0]], [p1[1], p2[1], p3[1], p1[1]], '-k')
-    plt.colorbar()
-    plt.show()
-
-
 def speed_comparison():
     # JIT vs non-JIT
     fig, ax = plt.subplots()
@@ -139,8 +103,8 @@ def speed_comparison():
 def visualize_function():
     # Plot individual function
 
-    buff = triangle.sample('nearest', points_f32, jit=False)
-    buff_2d = triangle2d.sample('nearest', points_f32, jit=False)
+    buff = triangle.sample('nearest', points_f64, jit=False)
+    buff_2d = triangle2d.sample('nearest', points_f64, jit=False)
     buff.shape = x.shape
     buff_2d.shape = x.shape
     diff = buff.T - buff_2d.T
@@ -173,4 +137,4 @@ def time_function():
 # test_cuda()
 # speed_comparison()
 visualize_function()
-time_function()
+# time_function()